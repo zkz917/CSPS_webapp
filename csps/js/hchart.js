889589e6f7381deca62fd195d5f314572e1d22f9
Kuangzheng Zhou <zkz917@126.com>
2016-10-14 11:12:26 -0400

    csps webapp

---
+++ csps/js/hchart.js
@@ -0,0 +1,168 @@
+$(function () {
+    $(document).ready(function () {
+        Highcharts.setOptions({
+            global: {
+                useUTC: false
+            }
+        });
+
+
+ 
+        $('#hchart').highcharts({
+            
+            
+            
+            chart: {
+                type: 'spline',
+                animation: Highcharts.svg, // don't animate in old IE
+                marginRight: 10,
+                width: 600,
+                height: 300,
+                events: {
+                    load: function () {
+                    var d = [1,1,2,2,1,2,1,2,1,3,4,6,8,2,2,30,41,18,15,16,11,12,13,14,15,16,20], i;
+                    
+                    var t = (new Date()).getTime()
+
+                        // set up the updating of the chart each second
+                        var series = this.series[0];
+                        
+                        setInterval(function () {
+                         var flag=true;
+                         
+                        
+                         var x = (new Date()).getTime(), // current time
+                                y = Math.round((x-t)/1000);
+                                if (y > 19) flag=false;
+                       
+                            series.addPoint([x, d[y]], flag, flag);
+                        
+                        
+                           
+                        }, 1000);
+                        
+                    }
+                }
+            },
+            title: {
+                text: 'Player #22 '
+            },
+            xAxis: {
+
+                type: 'datetime',
+                tickPixelInterval: 150
+            },
+            yAxis: {
+                title: {
+                    text: 'Impact data (G-Force)'
+                },
+                plotLines: [{
+                    value: 0,
+                    width: 1,
+                    color: '#808080'
+                }]
+            },
+            tooltip: {
+                formatter: function () {
+                    return '<b>' + this.series.name + '</b><br/>' +
+                        Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' +
+                        Highcharts.numberFormat(this.y, 2);
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            exporting: {
+                enabled: false
+            },
+             plotOptions: {
+            series: {
+                cursor: 'pointer',
+                point: {
+                    events: {
+
+                        click: function () {
+                            var vid = document.getElementById("Video1");
+
+                            var vid2 = document.getElementById("Video2");
+
+                            var yvalue = this.y;
+
+                            if(yvalue > 40){
+                            	vid.currentTime= 16;
+                            	vid.play();
+                            	
+                            	vid2.play();
+                            	
+
+                            }
+
+
+
+                            if(yvalue > 29){
+                            	vid.currentTime= 15;
+                            	vid.play();
+                            	
+                            	vid2.play();
+                            	
+
+                            }
+
+                            if(yvalue < 3){
+                            	vid.currentTime= 14;
+                            	vid.play();
+                            	
+                            	
+
+                            }
+                            if(yvalue < 2){
+                            	vid.currentTime= 1;
+                            	vid.play();
+                            	
+                            	
+
+                            }
+
+
+
+
+                            
+
+
+
+                            // alert('Category: ' + this.category + ', value: ' + this.y);
+
+
+
+                             //alert('The impact is 45G ');
+                             
+                            //vid.load();
+                            //vid.play();
+    
+                            
+
+                        }
+                    }
+                }
+            }
+        },
+            series: [{
+                name: 'Sensor data',
+                data: (function () {
+                    // generate an array of random data
+                    var data = [],
+                        time = (new Date()).getTime(),
+                        i;
+
+                    for (i = -20; i <= 0; i += 1) {
+                        data.push({
+                            x: time + i * 1000,
+                            y: 0
+                        });
+                    }
+                    return data;
+                }())
+            }]
+        });
+    });
+});
